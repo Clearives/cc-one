a6e23f3c532d229ce0fd22bb09c5d25a719b190e
clearives <clearives@gmail.com>
2017-06-10 11:53:22 +0800

    解决mongo新建表命名策略自动添加s问题

--- server/models/dbs.js
+++ server/models/dbs.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var models = require("./model");
 for(var m in models){
-    mongoose.model(m,new Schema(models[m]));
+    mongoose.model(m,new Schema(models[m]), m);
 }
 module.exports = {
     getModel:function (type) {
