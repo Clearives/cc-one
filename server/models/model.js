1a2da1a5d0a536b7a75535e48f3408f302adc9f6
clearives <clearives@gmail.com>
2017-06-10 11:49:27 +0800

    新增文章表

--- server/models/model.js
+++ server/models/model.js
@@ -2,5 +2,13 @@ module.exports = {
   user:{
       userid:{type:String,required:true},
       password:{type:String,required:true}
+  },
+  post: {
+    classify:{type:"string"},
+    title:{type:'string'},
+    content:{type:'string'},
+    contentToMark:{type:'string'}
   }
 };
+
+// module.exports.post.index({_id:1,classify:-1}).exec();
