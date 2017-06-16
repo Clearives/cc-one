f14dfe9c0c171a14c4490caed3d00ffe7998d870
clearives <clearives@gmail.com>
2017-06-16 09:13:00 +0800

    user 表结构修改

--- server/models/user.js
+++ server/models/user.js
@@ -3,8 +3,24 @@ const Schema = mongoose.Schema;
 
 
 const userSchema = new Schema({
-  userid: { type: String, required: true },
-  password: { type: String, required: true }
+  userid: { 
+  	type: String,
+  	required: true
+  },
+  password: {
+  	type: String,
+  	required: true
+  },
+  meta: {
+    createAt: {
+      type: Date,
+      default: Date.now()
+    },
+    updateAt: {
+      type: Date,
+      default: Date.now()
+    }
+  }
 })
 
 userSchema.statics = {
